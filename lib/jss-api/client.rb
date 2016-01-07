f8fddd0dee5c8ecd4c0236d9fe7ded7648f5cb49
Chris Lasell <chrisl@pixar.com>
2016-01-07 14:35:51 -0800

    cleanup

--- lib/jss-api/client.rb
+++ lib/jss-api/client.rb
@@ -1,25 +1,25 @@
 ### Copyright 2014 Pixar
-###  
+###
 ###    Licensed under the Apache License, Version 2.0 (the "Apache License")
 ###    with the following modification; you may not use this file except in
 ###    compliance with the Apache License and the following modification to it:
 ###    Section 6. Trademarks. is deleted and replaced with:
-###  
+###
 ###    6. Trademarks. This License does not grant permission to use the trade
 ###       names, trademarks, service marks, or product names of the Licensor
 ###       and its affiliates, except as required to comply with Section 4(c) of
 ###       the License and to reproduce the content of the NOTICE file.
-###  
+###
 ###    You may obtain a copy of the Apache License at
-###  
+###
 ###        http://www.apache.org/licenses/LICENSE-2.0
-###  
+###
 ###    Unless required by applicable law or agreed to in writing, software
 ###    distributed under the Apache License with the above modification is
 ###    distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 ###    KIND, either express or implied. See the Apache License for the specific
 ###    language governing permissions and limitations under the Apache License.
-### 
+###
 ###
 
 ###
@@ -109,7 +109,7 @@ module JSS
     #####################################
     ### Class Methods
     #####################################
-    
+
     ###
     ### Get the current IP address as a String.
     ###
@@ -126,7 +126,7 @@ module JSS
       ### turn off reverse DNS resolution temporarily
       ### @note the 'socket' library has already been required by 'rest-client'
       orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true
-  
+
       UDPSocket.open do |s|
         s.connect '192.168.0.0', 1
         s.addr.last
@@ -289,10 +289,10 @@ module JSS
         else
           raise JSS::InvalidDataError, "args must be a String or Array of Strings"
       end # case
-      
-      cmd += " -verbose" if verbose
+
+      cmd += " -verbose" if verbose and (not cmd.include? " -verbose")
       puts "Running: #{cmd}" if verbose
-      
+
       output = []
       IO.popen("#{cmd} 2>&1") do |proc|
         while line = proc.gets
--- lib/jss-api/utility.rb
+++ lib/jss-api/utility.rb
@@ -1,32 +1,32 @@
 ### Copyright 2014 Pixar
-###  
+###
 ###    Licensed under the Apache License, Version 2.0 (the "Apache License")
 ###    with the following modification; you may not use this file except in
 ###    compliance with the Apache License and the following modification to it:
 ###    Section 6. Trademarks. is deleted and replaced with:
-###  
+###
 ###    6. Trademarks. This License does not grant permission to use the trade
 ###       names, trademarks, service marks, or product names of the Licensor
 ###       and its affiliates, except as required to comply with Section 4(c) of
 ###       the License and to reproduce the content of the NOTICE file.
-###  
+###
 ###    You may obtain a copy of the Apache License at
-###  
+###
 ###        http://www.apache.org/licenses/LICENSE-2.0
-###  
+###
 ###    Unless required by applicable law or agreed to in writing, software
 ###    distributed under the Apache License with the above modification is
 ###    distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 ###    KIND, either express or implied. See the Apache License for the specific
 ###    language governing permissions and limitations under the Apache License.
-### 
+###
 ###
 
 ###
 module JSS
 
-  ### A collection of useful utility methods. Mostly for 
-  ### converting values between formats, parsing data, and 
+  ### A collection of useful utility methods. Mostly for
+  ### converting values between formats, parsing data, and
   ### user interaction.
 
   ### Converts an OS Version into an Array of higher OS versions.
@@ -92,7 +92,7 @@ module JSS
     end # each v
     return ok_oses
   end
-  
+
   ### Given a list of data as a comma-separated string, or an Array of strings,
   ### return a Hash with both versions.
   ###
@@ -125,7 +125,7 @@ module JSS
     end # case
     return {:stringform => valstr, :arrayform => valarr}
   end # to_s_and_a
-  
+
   ### parse a plist file, regardless of format, and return
   ### a Ruby data structure.
   ### This enhances Plist.parse_xml by duplicating and converting
@@ -196,14 +196,14 @@ module JSS
     Time.at the_dt.strftime('%s').to_i, usec
 
   end #parse_time
-  
-  ### Deprecated - to be eventually removed in favor of 
+
+  ### Deprecated - to be eventually removed in favor of
   ### the more-appropriately named JSS::parse_time
   ###
   ### @see JSS::parse_time
   ###
   def self.parse_datetime(a_datetime) ; self.parse_time(a_datetime) ; end
-  
+
   ### Converts JSS epoch (unix epoch + milliseconds) to a Ruby Time object
   ###
   ### @param epoch[String, Integer, nil]
@@ -419,6 +419,6 @@ module JSS
     end # begin
     return pw
   end
-  
+
 
 end # module
