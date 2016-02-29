ceaefded1a5be0c91cebc42bfeb07545cfa3ac39
Chris Lasell <chrisl@pixar.com>
2016-02-29 10:16:37 -0800

    api_connection.rb: open_uri and net/http don't seem to be happy with ssl_options like :OP_NO_SSLv2 and :OP_NO_SSLv3
    So until its resolved, I'm cheating and shelling out to curl to see if it says we need to log in.

--- lib/jss-api/api_connection.rb
+++ lib/jss-api/api_connection.rb
@@ -1,25 +1,25 @@
 ### Copyright 2016 Pixar
-###  
+###
 ###    Licensed under the Apache License, Version 2.0 (the "Apache License")
 ###    with the following modification; you may not use this file except in
 ###    compliance with the Apache License and the following modification to it:
 ###    Section 6. Trademarks. is deleted and replaced with:
-###  
+###
 ###    6. Trademarks. This License does not grant permission to use the trade
 ###       names, trademarks, service marks, or product names of the Licensor
 ###       and its affiliates, except as required to comply with Section 4(c) of
 ###       the License and to reproduce the content of the NOTICE file.
-###  
+###
 ###    You may obtain a copy of the Apache License at
-###  
+###
 ###        http://www.apache.org/licenses/LICENSE-2.0
-###  
+###
 ###    Unless required by applicable law or agreed to in writing, software
 ###    distributed under the Apache License with the above modification is
 ###    distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 ###    KIND, either express or implied. See the Apache License for the specific
 ###    language governing permissions and limitations under the Apache License.
-### 
+###
 ###
 
 ###
@@ -64,15 +64,16 @@ module JSS
 
     ### The base API path in the jss URL
     RSRC_BASE = "JSSResource"
-    
+
     ### A url path to load to see if there's an API available at a host.
     ### This just loads the API resource docs page
-    TEST_PATH = "api"
-    
+    TEST_PATH = "#{RSRC_BASE}/accounts"
+
     ### If the test path loads correctly from a casper server, it'll contain
-    ### this text
-    TEST_CONTENT = "<title>JSS REST API Resource Documentation</title>"
-    
+    ### this text (this is what we get when we make an unauthenticated
+    ### API call.)
+    TEST_CONTENT = "<p>The request requires user authentication</p>"
+
     ### The Default port
     HTTP_PORT = 9006
 
@@ -85,10 +86,10 @@ module JSS
     ### Default timeouts in seconds
     DFT_OPEN_TIMEOUT = 60
     DFT_TIMEOUT = 60
-    
+
     ### The Default SSL Version
     DFT_SSL_VERSION = 'TLSv1'
-    
+
     #####################################
     ### Attributes
     #####################################
@@ -107,7 +108,7 @@ module JSS
 
     ### @return [String] the hostname of the JSS to which we're connected.
     attr_reader :server_host
-    
+
     #####################################
     ### Constructor
     #####################################
@@ -143,8 +144,8 @@ module JSS
     ###
     ### @option args :pw[String,Symbol] Required, the password for that user, or :prompt, or :stdin
     ###   If :prompt, the user is promted on the commandline to enter the password for the :user.
-    ###   If :stdin#, the password is read from a line of std in represented by the digit at #, 
-    ###   so :stdin3 reads the passwd from the third line of standard input. defaults to line 1, 
+    ###   If :stdin#, the password is read from a line of std in represented by the digit at #,
+    ###   so :stdin3 reads the passwd from the third line of standard input. defaults to line 1,
     ###   if no digit is supplied. see {JSS.stdin}
     ###
     ### @option args :open_timeout[Integer] the number of seconds to wait for an initial response, defaults to 60
@@ -154,11 +155,11 @@ module JSS
     ### @return [true]
     ###
     def connect (args = {})
-      
+
       # the server, if not specified, might come from a couple places.
       # see #hostname
       args[:server] ||= hostname
-      
+
       # settings from config if they aren't in the args
       args[:server] ||= JSS::CONFIG.api_server_name
       args[:port] ||= JSS::CONFIG.api_server_port
@@ -172,31 +173,31 @@ module JSS
         # set it from the prefs
         args[:verify_cert] = JSS::CONFIG.api_verify_cert
       end
-      
+
       # settings from client jamf plist if needed
       args[:port] ||=  JSS::Client.jss_port
-      
+
       # default settings if needed
       args[:port] ||= SSL_PORT
       args[:timeout] ||= DFT_TIMEOUT
       args[:open_timeout] ||= DFT_OPEN_TIMEOUT
-      
+
       # As of Casper 9.61 we can't use SSL, must use TLS, since SSLv3 was susceptible to poodles.
       # NOTE - this requires rest-client v 1.7.0 or higher
       # which requires mime-types 2.0 or higher, which requires ruby 1.9.2 or higher!
       # That means that support for ruby 1.8.7 stops with Casper 9.6
       args[:ssl_version] ||= DFT_SSL_VERSION
-      
-      
+
+
       # must have server, user, and pw
       raise JSS::MissingDataError, "No JSS :server specified, or in configuration." unless args[:server]
       raise JSS::MissingDataError, "No JSS :user specified, or in configuration." unless args[:user]
       raise JSS::MissingDataError, "Missing :pw for user '#{args[:user]}'" unless args[:pw]
-      
+
       # we're using ssl if 1) args[:use_ssl] is anything but false
       # or 2) the port is the default casper ssl port.
       args[:use_ssl] = (not args[:use_ssl] == false) or (args[:port] == SSL_PORT)
-      
+
       # and here's the URL
       ssl = args[:use_ssl] ? "s" : ''
       @rest_url = URI::encode "http#{ssl}://#{args[:server]}:#{args[:port]}/#{RSRC_BASE}"
@@ -205,7 +206,7 @@ module JSS
       # prep the args for passing to RestClient::Resource
       # if verify_cert is anything but false, we will verify
       args[:verify_ssl] =  (args[:verify_cert] == false) ? OpenSSL::SSL::VERIFY_NONE : OpenSSL::SSL::VERIFY_PEER
-      
+
       args[:password] = if args[:pw] == :prompt
         JSS.prompt_for_password "Enter the password for JSS user #{args[:user]}@#{args[:server]}:"
       elsif args[:pw].is_a?(Symbol) and args[:pw].to_s.start_with?('stdin')
@@ -216,7 +217,7 @@ module JSS
       else
         args[:pw]
       end
-      
+
       # heres our connection
       @cnx = RestClient::Resource.new("#{@rest_url}", args)
 
@@ -347,23 +348,32 @@ module JSS
       @cnx[rsrc].delete
 
     end #delete_rsrc
-    
-    
+
+
     ### Test that a given hostname & port is a JSS API server
     ###
-    ### @param server[String] The hostname to test, 
+    ### @param server[String] The hostname to test,
     ###
     ### @param port[Integer] The port to try connecting on
     ###
     ### @return [Boolean] does the server host a JSS API?
     ###
     def valid_server? (server, port = SSL_PORT)
+      # cheating by shelling out to curl, because getting open-uri, or even net/http to use
+      # ssl_options like :OP_NO_SSLv2 and :OP_NO_SSLv3 will take time to figure out..
+      return true if `/usr/bin/curl -s 'https://#{server}:#{port}/#{TEST_PATH}'`.include? TEST_CONTENT
+      return true if `/usr/bin/curl -s 'http://#{server}:#{port}/#{TEST_PATH}'`.include? TEST_CONTENT
+      return false
+
       # try ssl first
+      # NOTE:  doesn't work if we can't disallow SSLv3 or force TLSv1
+      # See cheat above.
       begin
         return true if open("https://#{server}:#{port}/#{TEST_PATH}", ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE).read.include? TEST_CONTENT
+
       rescue
         # then regular http
-        begin 
+        begin
           return true if open("http://#{server}:#{port}/#{TEST_PATH}").read.include? TEST_CONTENT
         rescue
           # any errors = no API
@@ -373,8 +383,8 @@ module JSS
       # if we're here, no API
       return false
     end
-    
-    ### The server to which we are connected, or will 
+
+    ### The server to which we are connected, or will
     ### try connecting to if none is specified with the
     ### call to #connect
     ###
@@ -386,7 +396,7 @@ module JSS
       srvr ||= JSS::Client.jss_server
       return srvr
     end
-    
+
     ### aliases
     alias connected? connected
 
