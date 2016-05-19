7d6cc336842180209528ca6a05237bbf254f89b8
Steven Scott <steven.scott@shopify.com>
2016-05-19 16:29:28 -0400

    adding attributes to restricted software

--- lib/jss/api_object/restricted_software.rb
+++ lib/jss/api_object/restricted_software.rb
@@ -37,5 +37,50 @@ module JSS
 
     ### Our scopes deal with computers
     SCOPE_TARGET_KEY = :computers
+
+    #####################################
+    ### Attributes
+    #####################################
+
+    ### The values returned in the General, Location, and Purchasing subsets are stored as direct attributes
+    ### Location and Purchasing are defined in the Locatable and Purchasable mixin modules.
+    ### Here's General, in alphabetical order
+
+    ### @return [String] the process name
+    attr_reader :process_name
+
+    ### @return [Boolean] whether to return match exact process name
+    attr_reader :match_exact_process_name
+
+    ### @return [Boolean] whether to send a notification
+    attr_reader :send_notification
+
+    ### @return [Boolean] whether to kill the running process
+    attr_reader :kill_process
+
+    ### @return [Boolean] whether to delete the executable
+    attr_reader :delete_executable
+
+    ### @return [String] message displayed to the user
+    attr_reader :display_message
+
+    ### @return [Hash] the :name and :id of the site for this machine
+    attr_reader :site
+
+    #####################################
+    ### Instance Methods
+    #####################################
+
+    def initialize(args = {})
+      super args, []
+
+      @process_name = @init_data[:general][:process_name]
+      @match_exact_process_name = @init_data[:general][:match_exact_process_name]
+      @send_notification = @init_data[:general][:send_notification]
+      @kill_process = @init_data[:general][:kill_process]
+      @delete_executable = @init_data[:general][:delete_executable]
+      @display_message = @init_data[:general][:display_message]
+      @site = JSS::APIObject.get_name(@init_data[:general][:site])
+    end
   end
 end
