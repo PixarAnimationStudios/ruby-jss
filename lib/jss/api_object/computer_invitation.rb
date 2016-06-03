a8f8843a7b3baf93a1658ee05528381b0d401f72
Steven Scott <steven.scott@shopify.com>
2016-06-03 00:28:39 -0400

    trying to re-add epoch

--- lib/jss/api_object/computer_invitation.rb
+++ lib/jss/api_object/computer_invitation.rb
@@ -104,7 +104,7 @@ module JSS
                            name: "some_new_name",
                            invitation_type: "URL",
                            create_account_if_does_not_exist: "true",
-                           expiration_date_epoch: "Unlimited"})
+                           expiration_date_epoch: "1465036088731"})
 
       super args, []
 
@@ -145,7 +145,7 @@ module JSS
       obj = doc.add_element RSRC_OBJECT_KEY.to_s
       obj.add_element('invitation_type').text = @invitation_type
       obj.add_element('create_account_if_does_not_exist').text = @create_account_if_does_not_exist
-      #obj.add_element('expiration_date_epoch').text = @expiration_date_epoch
+      obj.add_element('expiration_date_epoch').text = @expiration_date_epoch
 
       return doc.to_s
     end
