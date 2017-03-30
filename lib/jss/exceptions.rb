9bada7be932c91f8ca0a75b7cbf623bc395391c0
Chris Lasell <chrisl@pixar.com>
2017-03-30 16:13:14 -0700

    added ConflictError exception class

--- lib/jss/exceptions.rb
+++ lib/jss/exceptions.rb
@@ -1,96 +1,104 @@
 ### Copyright 2017 Pixar
 
-###  
+###
 ###    Licensed under the Apache License, Version 2.0 (the "Apache License")
 ###    with the following modification; you may not use this file except in
 ###    compliance with the Apache License and the following modification to it:
 ###    Section 6. Trademarks. is deleted and replaced with:
-###  
+###
 ###    6. Trademarks. This License does not grant permission to use the trade
 ###       names, trademarks, service marks, or product names of the Licensor
 ###       and its affiliates, except as required to comply with Section 4(c) of
 ###       the License and to reproduce the content of the NOTICE file.
-###  
+###
 ###    You may obtain a copy of the Apache License at
-###  
+###
 ###        http://www.apache.org/licenses/LICENSE-2.0
-###  
+###
 ###    Unless required by applicable law or agreed to in writing, software
 ###    distributed under the Apache License with the above modification is
 ###    distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 ###    KIND, either express or implied. See the Apache License for the specific
 ###    language governing permissions and limitations under the Apache License.
-### 
+###
 ###
 
 ###
 module JSS
-  
+
   #####################################
   ### Exceptions
   #####################################
-  
-  ### 
-  ### MissingDataError - raise this error when we 
+
+  ###
+  ### MissingDataError - raise this error when we
   ### are missing args, or other simliar stuff.
-  ### 
+  ###
   class MissingDataError < RuntimeError; end
-  
-  ### 
-  ### InvalidDataError - raise this error when 
+
+  ###
+  ### InvalidDataError - raise this error when
   ### a data item isn't what we expected.
-  ### 
+  ###
   class InvalidDataError < RuntimeError; end
-  
-  ### 
-  ### InvalidConnectionError - raise this error when we 
+
+  ###
+  ### InvalidConnectionError - raise this error when we
   ### don't have a usable connection to a network service, or
   ### don't have proper authentication/authorization.
-  ### 
+  ###
   class InvalidConnectionError < RuntimeError; end
-  
-  ### 
-  ### NoSuchItemError - raise this error when 
+
+  ###
+  ### NoSuchItemError - raise this error when
   ### a desired item doesn't exist.
-  ### 
+  ###
   class NoSuchItemError < RuntimeError; end
-  
-  ### 
-  ### AlreadyExistsError - raise this error when 
+
+  ###
+  ### AlreadyExistsError - raise this error when
   ### trying to create something that already exists.
-  ### 
+  ###
   class AlreadyExistsError < RuntimeError; end
-  
-  ### 
-  ### FileServiceError - raise this error when 
+
+  ###
+  ### FileServiceError - raise this error when
   ### there's a problem accessing file service on a
   ### distribution point.
-  ### 
-  class FileServiceError < RuntimeError; end
-  
   ###
-  ### UnmanagedError - raise this when we 
-  ### try to do something managerial to 
+  class FileServiceError < RuntimeError; end
+
+  ###
+  ### UnmanagedError - raise this when we
+  ### try to do something managerial to
   ### an unmanaged object
   ###
   class UnmanagedError <  RuntimeError; end
-  
+
   ###
-  ### UnsupportedError - raise this when we 
+  ### UnsupportedError - raise this when we
   ### try to do something not yet supported
   ###
   class UnsupportedError <  RuntimeError; end
-  
+
   ###
-  ### TimeoutError - raise this when we 
+  ### TimeoutError - raise this when we
   ### try to do and it times out
   ###
   class TimeoutError <  RuntimeError; end
-  
+
   ###
-  ### AuthenticationError - raise this when 
+  ### AuthenticationError - raise this when
   ### a name/pw are wrong
   ###
   class AuthenticationError <  RuntimeError; end
-  
-end # module JSS
+
+  ###
+  ### ConflictError - raise this when
+  ### attempts to PUT or PUSH to the API
+  ### result in a 409 Conflict http error.
+  ### See JSS::APIConnect.instance.raise_conflict_error
+  ###
+  class ConflictError <  RuntimeError; end
+
+end  # module JSS
