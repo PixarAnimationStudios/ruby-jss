ad45ea5a2ae6efd3dc585c9b531283b187af3ae4
Chris Lasell <chrisl@pixar.com>
2017-07-06 16:44:08 -0700

    JSS::MobileDevice - mgmt history and better mdm commands

--- lib/jss/api_object/mobile_device.rb
+++ lib/jss/api_object/mobile_device.rb
@@ -120,27 +120,73 @@ module JSS
     # This is the class for relevant Extension Attributes
     EXT_ATTRIB_CLASS = JSS::MobileDeviceExtensionAttribute
 
-    #######
+    # the rsrc for mobile dev commands
+    MDM_RSRC = 'mobiledevicecommands/command'.freeze
+
     # The MDM commands sendable via the api
     # and alternative versions
     #
-    MOBILE_DEV_MDM_COMMANDS = {
+    MDM_COMMANDS = {
       blank_push: 'BlankPush',
       send_blank_push: 'BlankPush',
       blank: 'BlankPush',
       noop: 'BlankPush',
+
+      settings: 'Settings',
+
       update_inventory: 'UpdateInventory',
       recon: 'UpdateInventory',
+
       device_lock: 'DeviceLock',
       lock: 'DeviceLock',
       lock_device: 'DeviceLock',
+
       erase_device: 'EraseDevice',
       erase: 'EraseDevice',
       wipe: 'EraseDevice',
+
       clear_passcode: 'ClearPasscode',
+
+      clear_restrictions_password: 'ClearRestrictionsPassword',
+
+      enable_data_roaming: 'SettingsEnableDataRoaming',
+      disable_data_roaming: 'SettingsDisableDataRoaming',
+
+      enable_voice_roaming: 'SettingsEnableVoiceRoaming',
+      disable_voice_roaming: 'SettingsDisableVoiceRoaming',
+
+      enable_app_analytics: 'SettingsEnableAppAnalytics',
+      disable_app_analytics: 'SettingsDisableAppAnalytics',
+
+      enable_diagnostic_submission: 'SettingsEnableDiagnosticSubmission',
+      disable_diagnostic_submission: 'SettingsDisableDiagnosticSubmission',
+
+      # wallpaper: 'Wallpaper',
+
+      device_name: 'DeviceName',
+
+      shutdown_device: 'ShutDownDevice',
+      shutdown: 'ShutDownDevice',
+
+      restart_device: 'RestartDevice',
+      restart: 'RestartDevice',
+
+      # passcode_lock_grace_period: 'PasscodeLockGracePeriod',
+
       unmanage_device: 'UnmanageDevice',
       unmanage: 'UnmanageDevice'
+
     }.freeze
+
+    # These MDM commands need extra data.
+    MDM_COMMANDS_REQUIRING_DATA = %w[DeviceLock DeviceName Wallpaper].freeze
+
+    # The History resource
+    HISTORY_RSRC = 'mobiledevicehistory'.freeze
+
+    # Available history subsets
+    HISTORY_SUBSETS = %i[management_commands user_location audits applications ebooks].freeze
+
 
     # Class Methods
     #####################################
@@ -158,6 +204,11 @@ module JSS
     # @return [Array<String>] all mobiledevice wifi mac addrs
     def self.all_wifi_mac_addresses(refresh = false)
       all(refresh).map { |i| i[:wifi_mac_address] }
+    end
+
+    # @return [Array<String>] all mobiledevice wifi mac addrs
+    def self.all_mac_addresses(refresh = false)
+      all_wifi_mac_addresses(refresh)
     end
 
     # @return [Array<String>] all mobiledevice udids
@@ -190,37 +241,79 @@ module JSS
       all(refresh).select { |d| d[:model_identifier].start_with? 'AppleTV' }
     end
 
-    # Send an MDM command to a managed mobile device.
+    # Send an MDM command to one or more mobile devices by id or name
     #
-    # @param dev[Integer,String] the id, name, serialnum, udid, phone num, or wifi macaddr of the device
     #
-    # @param command[Symbol] the command to send, one of the keys of MOBILE_DEV_MDM_COMMANDS
+    # @param targets[String,Integer,Array<String,Integer>]
+    #   the name or id of the mobile devices to receive the command, or
+    #   an array of such names or ids, or a comma-separated string
+    #   of them.
+    # @param command[Symbol] the command to send, one of the keys
+    #   of MDM_COMMANDS
     #
-    # @return [Boolean] true if command was sent successfully
+    # @param data[String] Some commands require extra data.
     #
-    # See also {#blank_push}, {#update_inventory}, {#device_lock},
-    # {#erase_device}, {#clear_passcode}, and {#unmanage_device}
+    # @return [String] The uuid of the MDM command sent, if applicable
+    #  (blank pushes do not generate uuids)
     #
-    def self.send_mdm_command(dev, command)
-      raise JSS::NoSuchItemError, "Unknown command '#{command}'" unless MOBILE_DEV_MDM_COMMANDS.keys.include? command
-      command_xml = "#{JSS::APIConnection::XML_HEADER}<mobile_device><command>#{MOBILE_DEV_MDM_COMMANDS[command]}</command></mobile_device>"
-      the_id = nil
-      all_managed.each do |mmd|
-        if [mmd[:id], mmd[:name], mmd[:serial_number], mmd[:phone_number], mmd[:udid], mmd[:wifi_mac_address]].include? dev
-          the_id = mmd[:id]
-          break
-        end
-      end # each do mmd
+    def self.send_mdm_command(targets, command, data = nil)
+      raise JSS::NoSuchItemError, "Unknown command '#{command}'" unless MDM_COMMANDS.keys.include? command
 
-      if the_id
-        response = JSS.api_connection.put_rsrc("#{RSRC_BASE}/id/#{the_id}", command_xml)
-        response =~ %r{<notification_sent>(.+)</notification_sent>}
-        return (Regexp.last_match(1) && Regexp.last_match(1) == 'true')
+      command = MDM_COMMANDS[command]
+      cmd_rsrc = "#{MDM_RSRC}/#{command}"
+
+      if MDM_COMMANDS_REQUIRING_DATA.include? command
+        raise "MDM command '#{command}' requires additional data." unless data
+        cmd_rsrc << "/#{data}"
       end
-      raise JSS::UnmanagedError, "Cannot send command to unknown/unmanaged device '#{dev}'"
+
+      targets = JSS.to_s_and_a(targets.to_s)[:arrayform] unless targets.is_a? Array
+
+      # make sure its an array of ids
+      targets.map! do |md|
+        if all_ids.include? md.to_i
+          md.to_i
+        elsif all_names.include? md
+          map_all_ids_to(:name).invert[md]
+        else
+          raise JSS::NoSuchItemError, "No mobile device found matching '#{md}'"
+        end # if
+      end # map!
+
+      cmd_rsrc << "/id/#{targets.join ','}"
+
+      result = JSS::API.post_rsrc cmd_rsrc, nil
+      result =~ %r{<uuid>(.*)</uuid>}
+      Regexp.last_match(1)
     end
 
-    #####################################
+    def self.management_history(identifier, subset = nil )
+      id = nil
+      if identifier.is_a? Integer
+        id = identifier
+      else
+        key = case identifier
+              when *all_names then :name
+              when *all_serial_numbers then :serial_number
+              when *all_mac_addresses then :mac_address
+              when *all_udids then :udid
+              end
+        id = map_all_ids_to(key).invert[identifier]
+      end # if identifier.is_a? Integer
+
+      raise JSS::NoSuchItemError, "No MobileDevice found matching #{identifier}" unless id && all_ids.include?(id)
+
+      rsrc = "#{HISTORY_RSRC}/id/#{id}"
+
+      if subset
+        raise "subset must be one of: :#{HISTORY_SUBSETS.join ', :'}" unless HISTORY_SUBSETS.include? subset
+        rsrc << "/subset/#{subset}"
+      end
+
+      hist = JSS.api.get_rsrc(rsrc)[:mobile_device_history]
+      subset ? hist[subset] : hist
+    end
+
     # Attributes
     #####################################
 
@@ -447,12 +540,11 @@ module JSS
       @applications = @init_data[:applications]
     end # initialize
 
-    #
     # Send a blank_push MDM command
     #
     # @see MobileDevice.send_mdm_command
     #
-    # @return [Boolean]
+    # @return [String] The command uuid
     #
     def blank_push
       self.class.send_mdm_command @id, :blank_push
@@ -462,55 +554,365 @@ module JSS
     #
     # @see MobileDevice.send_mdm_command
     #
-    # @return [Boolean]
+    # @return [String] The command uuid
     #
     def update_inventory
       self.class.send_mdm_command @id, :update_inventory
     end
 
-    #
     # Send a device_lock MDM command
+    #
+    # @param message[String] The message to display on the lock screen.
     #
     # @see MobileDevice.send_mdm_command
     #
-    # @return [Boolean]
+    # @return [String] The command uuid
     #
-    def device_lock
-      self.class.send_mdm_command @id, :device_lock
+    def device_lock(message)
+      self.class.send_mdm_command @id, :device_lock, message
     end
 
-    #
     # Send an erase_device MDM command
     #
     # @see MobileDevice.send_mdm_command
     #
-    # @return [Boolean]
+    # @return [String] The command uuid
     #
     def erase_device
       self.class.send_mdm_command @id, :erase_device
     end
 
-    #
     # Send a clear_passcode MDM command
     #
     # @see MobileDevice.send_mdm_command
     #
-    # @return [Boolean]
+    # @return [String] The command uuid
     #
     def clear_passcode
       self.class.send_mdm_command @id, :clear_passcode
     end
 
-    #
     # Send a unmanage_device MDM command
     #
     # @see MobileDevice.send_mdm_command
     #
-    # @return [Boolean]
+    # @return [String] The command uuid
     #
     def unmanage_device
       @managed = false if self.class.send_mdm_command(@id, :unmanage_device)
     end
+
+    # Send a ClearRestrictionsPassword MDM command
+    #
+    # @see MobileDevice.send_mdm_command
+    #
+    # @return [String] The command uuid
+    #
+    def clear_restrictions_password
+      self.class.send_mdm_command @id, :clear_restrictions_password
+    end
+
+    # Send a SettingsEnableDataRoaming MDM command
+    #
+    # @see MobileDevice.send_mdm_command
+    #
+    # @return [String] The command uuid
+    #
+    def enable_data_roaming
+      self.class.send_mdm_command @id, :enable_data_roaming
+    end
+
+    # Send a disable_data_roaming MDM command
+    #
+    # @see MobileDevice.send_mdm_command
+    #
+    # @return [String] The command uuid
+    #
+    def disable_data_roaming
+      self.class.send_mdm_command @id, :disable_data_roaming
+    end
+
+    # Send a enable_voice_roaming MDM command
+    #
+    # @see MobileDevice.send_mdm_command
+    #
+    # @return [String] The command uuid
+    #
+    def enable_voice_roaming
+      self.class.send_mdm_command @id, :enable_voice_roaming
+    end
+
+    # Send a disable_voice_roaming MDM command
+    #
+    # @see MobileDevice.send_mdm_command
+    #
+    # @return [String] The command uuid
+    #
+    def disable_voice_roaming
+      self.class.send_mdm_command @id, :disable_voice_roaming
+    end
+
+    # Send a enable_app_analytics MDM command
+    #
+    # @see MobileDevice.send_mdm_command
+    #
+    # @return [String] The command uuid
+    #
+    def enable_app_analytics
+      self.class.send_mdm_command @id, :enable_app_analytics
+    end
+
+    # Send a disable_app_analytics MDM command
+    #
+    # @see MobileDevice.send_mdm_command
+    #
+    # @return [String] The command uuid
+    #
+    def disable_app_analytics
+      self.class.send_mdm_command @id, :disable_app_analytics
+    end
+
+    # Send a enable_diagnostic_submission MDM command
+    #
+    # @see MobileDevice.send_mdm_command
+    #
+    # @return [String] The command uuid
+    #
+    def enable_diagnostic_submission
+      self.class.send_mdm_command @id, :enable_diagnostic_submission
+    end
+
+    # Send a disable_diagnostic_submission MDM command
+    #
+    # @see MobileDevice.send_mdm_command
+    #
+    # @return [String] The command uuid
+    #
+    def disable_diagnostic_submission
+      self.class.send_mdm_command @id, :disable_diagnostic_submission
+    end
+
+    # Send a device_name MDM command
+    #
+    # @param new_name[String] The name for the device
+    #
+    # @see MobileDevice.send_mdm_command
+    #
+    # @return [String] The command uuid
+    #
+    def device_name(new_name)
+      self.class.send_mdm_command @id, :device_name, new_name
+    end
+
+    # Send a shutdown device MDM command
+    #
+    # @see MobileDevice.send_mdm_command
+    #
+    # @return [String] The command uuid
+    #
+    def shutdown
+      self.class.send_mdm_command @id, :shutdown
+    end
+
+    # Send a restart device MDM command
+    #
+    # @see MobileDevice.send_mdm_command
+    #
+    # @return [String] The command uuid
+    #
+    def restart
+      self.class.send_mdm_command @id, :restart
+    end
+
+
+    # The full management History data for this Mobile Device
+    #
+    # @return [Hash] Keys are:
+    #   general: Hash of identifiers
+    #   user_location: see user_location_history
+    #   management_commands: see management_command_history
+    #   audits:  see audit_history
+    #   applications: see managed_app_history
+    #   ebooks: see managed
+    #
+    def management_history
+      self.class.management_history @id
+    end
+
+    # The user_location subset of the full history
+    #
+    # @return [Array<Hash>] Each hash contains user and location data
+    #   and the timestamp as a JSS epoch value or string.
+    #   use JSS.epoch_to_time or JSS.parse_time to convert them to Time
+    #   objects
+    #
+    def user_location_history
+      self.class.management_history @id, :user_location
+    end
+
+    # The management_commands subset of the full history
+    #
+    # @return [Hash] A hash of :completed, :pending, and :failed
+    #   mdm commands, each being an array of hashes.
+    #   see #completed_mdm_commands, #pending_mdm_commands and
+    #   #failed_mdm_commands
+    #
+    def management_command_history
+      self.class.management_history @id, :management_commands
+    end
+    alias mdm_command_history management_command_history
+
+    # The history of completed mdm commands.
+    #
+    # @return [Array<Hash>] Each hash contains a command name and
+    #   and the timestamp as a JSS epoch value or string.
+    #   use JSS.epoch_to_time or JSS.parse_time to convert them to Time
+    #   objects
+    #
+    def completed_mdm_commands
+      management_command_history[:completed]
+    end
+
+    # The currently pending mdm commands.
+    #
+    # @return [Array<Hash>] Each hash contains a command name and a :status
+    #   and a a timestamp as a JSS epoch value or string.
+    #   use JSS.epoch_to_time or JSS.parse_time to convert them to Time
+    #   objects
+    #
+    def pending_mdm_commands
+      management_command_history[:pending]
+    end
+
+    # The history of failed mdm commands.
+    #
+    # @return [Array<Hash>] Each hash contains a command name and an :error
+    #   message and timestamps for issuance and failure as JSS epoch values
+    #   or strings. use JSS.epoch_to_time or JSS.parse_time to convert them to
+    #   Time objects
+    #
+    def failed_mdm_commands
+      management_command_history[:failed]
+    end
+
+    # The applications subset of the full history
+    #
+    # @return [Hash] Keys are :installed, :pending, and :failed
+    #   See #installed_managed_apps, #pending_managed_apps and
+    #   #failed_managed_apps
+    #
+    def managed_app_history
+      self.class.management_history @id, :applications
+    end
+
+    # The apps that have been installed via MDM
+    #
+    # @param from[Symbol] :in_house, :app_store, :other, or :all.
+    #   Defaults to :all
+    #
+    # @return [Hash{Array<Hash>}] When from = :all, all three sources.
+    #
+    # @return [Array<Hash>] When from = :in_house, :app_store, or :other
+    #   the managed apps that have been installed from that source.
+    #   Each Hash includes these keys:
+    #     name:
+    #     version:
+    #     short_version:
+    #     management_status:
+    #     bundle_size:
+    #     dynamic_size:
+    #
+    def installed_managed_apps(from = :all)
+      all = managed_app_history[:installed]
+      case from
+      when :all
+        all
+      when :in_house
+        all[:in_house_from_mobile_device_app_catalog]
+      when :app_store
+        all[:app_store_from_mobile_device_app_catalog]
+      when :other
+        all[:other]
+      else
+        raise JSS::InvalidDataError, "Unknown mobiledevice app source: '#{from}'"
+      end
+    end
+
+    # An array of pending managed app installs.
+    #
+    # @return [Array<Hash>] Hashes for each pending app install
+    #
+    def pending_managed_apps
+      managed_app_history[:pending]
+    end
+
+    # An array of data about failed managed app installs
+    #
+    # @return [Array<Hash>] The name: and version: of each failed app install.
+    #
+    def failed_managed_apps
+      managed_app_history[:failed]
+    end
+
+    # The audits subset of the full history
+    # The history of 'auditable' events.
+    #
+    # @return [Array<Hash>] One hash per event
+    #
+    def audit_history
+      self.class.management_history @id, :audits
+    end
+
+    # The ebooks subset of the full history
+    #
+    # @return [Hash] Keys are :installed, :pending, and :failed
+    #   See #installed_managed_ebooks, #pending_managed_ebooks and
+    #   #failed_managed_ebooks
+    #
+    def managed_ebook_history
+      self.class.management_history @id, :ebooks
+    end
+
+    # The ebooks that have been installed via MDM
+    #
+    # @param from[Symbol] :in_house, :ibookstore, or :all.
+    #   Defaults to :all
+    #
+    # @return [Hash{Array<Hash>}] When from = :all, all three sources.
+    #
+    # @return [Array<Hash>] When from = :in_house, :app_store, or :other
+    #   the managed apps that have been installed from that source.
+    #
+    def installed_managed_ebooks(from = :all)
+      all = managed_ebook_history[:installed]
+      case from
+      when :all
+        all
+      when :in_house
+        all[:inhouse]
+      when :ibookstore
+        all[:ibookstore]
+      else
+        raise JSS::InvalidDataError, "Unknown ebook source: '#{from}'"
+      end
+    end
+
+    # Mananged ebooks pending installation
+    #
+    # @return [Array<Hash>] The pending ebooks
+    #
+    def pending_managed_ebooks
+      managed_ebook_history[:pending]
+    end
+
+   # Mananged ebooks tha failed installation
+   #
+   # @return [Array<Hash>] The failed ebook installs
+   #
+    def failed_managed_ebooks
+      managed_ebook_history[:failed]
+    end
+
 
     # Aliases
     alias battery_percent battery_level
@@ -528,7 +930,6 @@ module JSS
     alias unmanage unmanage_device
     alias make_unmanaged unmanage_device
 
-    ##############################
     # private methods
     ##############################
 
