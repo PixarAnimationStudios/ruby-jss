51112f4a8838ac836f11ab4f08b7a1f3c100d1e1
Chris Lasell <chrisl@pixar.com>
2017-10-04 15:19:59 -0700

    more precise error messages

--- lib/jss/api_object.rb
+++ lib/jss/api_object.rb
@@ -610,10 +610,10 @@ module JSS
     #
     def save
       if @in_jss
-        raise JSS::UnsupportedError, 'Updating this object in the JSS is currently not supported' unless updatable?
+        raise JSS::UnsupportedError, 'Updating this object in the JSS is currently not supported by ruby-jss' unless updatable?
         update
       else
-        raise JSS::UnsupportedError, 'Creating this object in the JSS is currently not supported' unless creatable?
+        raise JSS::UnsupportedError, 'Creating this object in the JSS is currently not supported by ruby-jss' unless creatable?
         create
       end
     end
