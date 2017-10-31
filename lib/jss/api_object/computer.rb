61ad59c809651db834be39f6f78b814aed5e37b1
Chris Lasell <chrisl@pixar.com>
2017-10-31 13:48:07 -0700

    same, but better

--- lib/jss/api_object/computer.rb
+++ lib/jss/api_object/computer.rb
@@ -1161,14 +1161,14 @@ module JSS
     #
     def serial_number=(new_val)
       return nil if new_val == @serial_number
-      @serial_number =  new_val.empty? ? new_val : JSS::Validate.unique_identifier(JSS::Computer, :serial_number, new_val, api)
+      @serial_number =  new_val.empty? ? new_val : JSS::Validate.unique_identifier(JSS::Computer, :serial_number, new_val, api: api)
       @need_to_update = true
     end
 
     #
     def udid=(new_val)
       return nil if new_val == @udid
-      @udid = new_val.empty? ? new_val : JSS::Validate.unique_identifier(JSS::Computer, :udid, new_val, api)
+      @udid = new_val.empty? ? new_val : JSS::Validate.unique_identifier(JSS::Computer, :udid, new_val, api: api)
       @need_to_update = true
     end
 
--- lib/jss/validate.rb
+++ lib/jss/validate.rb
@@ -76,7 +76,7 @@ module JSS
     # @return [Object] the validated unique value
     #
     def self.unique_identifier(klass, identifier, val, api: JSS.api)
-      raise JSS::AlreadyExistsError, "A #{klass} already exists with #{identifier} '#{val}'" if klass.all(api: api).map { |i| i[identifier] }.include? val
+      raise JSS::AlreadyExistsError, "A #{klass} already exists with #{identifier} '#{val}'" if klass.all(:refresh, api: api).map { |i| i[identifier] }.include? val
       val
     end
 
