964b7f19d3ab0647cc155754e2eb93e348baffd3
Chris Lasell <chrisl@pixar.com>
2017-12-01 09:50:08 -0800

    rubocop appeasement, for better code

--- lib/jss/api_object/scopable/scope.rb
+++ lib/jss/api_object/scopable/scope.rb
@@ -28,9 +28,9 @@ module JSS
 
   module Scopable
 
-  #####################################
-  ### Classes
-  #####################################
+    #####################################
+    ### Classes
+    #####################################
 
     ###
     ### This class represents a Scope in the JSS, as can be applied to Scopable objects like
@@ -67,55 +67,53 @@ module JSS
 
       ### These are the classes that Scopes can use for defining a scope,
       ### keyed by appropriate symbols.
-      SCOPING_CLASSES ={
-        :computers => JSS::Computer,
-        :computer => JSS::Computer,
-        :computer_groups => JSS::ComputerGroup,
-        :computer_group => JSS::ComputerGroup,
-        :mobile_devices => JSS::MobileDevice,
-        :mobile_device => JSS::MobileDevice,
-        :mobile_device_groups => JSS::MobileDeviceGroup,
-        :mobile_device_group => JSS::MobileDeviceGroup,
-        :buildings => JSS::Building,
-        :building => JSS::Building,
-        :departments => JSS::Department,
-        :department => JSS::Department,
-        :network_segments => JSS::NetworkSegment,
-        :network_segment => JSS::NetworkSegment,
-        :users => JSS::User,
-        :user => JSS::User,
-        :user_groups => JSS::UserGroup,
-        :user_group => JSS::UserGroup
-      }
+      SCOPING_CLASSES = {
+        computers: JSS::Computer,
+        computer: JSS::Computer,
+        computer_groups: JSS::ComputerGroup,
+        computer_group: JSS::ComputerGroup,
+        mobile_devices: JSS::MobileDevice,
+        mobile_device: JSS::MobileDevice,
+        mobile_device_groups: JSS::MobileDeviceGroup,
+        mobile_device_group: JSS::MobileDeviceGroup,
+        buildings: JSS::Building,
+        building: JSS::Building,
+        departments: JSS::Department,
+        department: JSS::Department,
+        network_segments: JSS::NetworkSegment,
+        network_segment: JSS::NetworkSegment,
+        users: JSS::User,
+        user: JSS::User,
+        user_groups: JSS::UserGroup,
+        user_group: JSS::UserGroup
+      }.freeze
 
       ### Some things get checked in LDAP as well as the JSS
-      LDAP_USER_KEYS = [:user, :users]
-      LDAP_GROUP_KEYS = [:user_groups, :user_group]
+      LDAP_USER_KEYS = %i[user users].freeze
+      LDAP_GROUP_KEYS = %i[user_groups user_group].freeze
       CHECK_LDAP_KEYS = LDAP_USER_KEYS + LDAP_GROUP_KEYS
 
       ### This hash maps the availble Scope Target keys from SCOPING_CLASSES to
       ### their corresponding target group keys from SCOPING_CLASSES.
-      TARGETS_AND_GROUPS = {:computers => :computer_groups, :mobile_devices => :mobile_device_groups }
+      TARGETS_AND_GROUPS = { computers: :computer_groups, mobile_devices: :mobile_device_groups }.freeze
 
       ### These can be part of the base inclusion list of the scope,
       ### along with the appropriate target and target group keys
-      INCLUSIONS = [:buildings, :departments]
+      INCLUSIONS = %i[buildings departments].freeze
 
       ### These can limit the inclusion list
-      LIMITATIONS = [:network_segments, :users, :user_groups]
+      LIMITATIONS = %i[network_segments users user_groups].freeze
 
       ### any of them can be excluded
       EXCLUSIONS = INCLUSIONS + LIMITATIONS
 
       ### Here's a default scope as it might come from the API.
       DEFAULT_SCOPE = {
-        :all_computers => true,
-        :all_mobile_devices => true,
-        :limitations => {},
-        :exclusions => {}
-      }
-
-
+        all_computers: true,
+        all_mobile_devices: true,
+        limitations: {},
+        exclusions: {}
+      }.freeze
 
       ######################
       ### Attributes
@@ -158,8 +156,6 @@ module JSS
       ###
       attr_reader :all_targets
 
-
-
       ### @return [Hash<Array>]
       ###
       ### The items in these arrays are the limitations applied to targets in the @inclusions .
@@ -185,7 +181,6 @@ module JSS
       ### - :user_groups
       ###
       attr_reader :exclusions
-
 
       #####################################
       ### Public Instance Methods
@@ -217,21 +212,20 @@ module JSS
         ### Everything gets mapped from an Array of Hashes to an Array of names (or an empty array)
         ### since names are all that really matter when submitting the scope.
         @inclusions = {}
-        @inclusion_keys.each{|k| @inclusions[k] = api_scope[k] ? api_scope[k].map{|n| n[:name]} :  [] }
+        @inclusion_keys.each { |k| @inclusions[k] = api_scope[k] ? api_scope[k].map { |n| n[:name] } : [] }
 
         @limitations = {}
         if api_scope[:limitations]
-          LIMITATIONS.each{|k| @limitations[k] = api_scope[:limitations][k] ? api_scope[:limitations][k].map{|n| n[:name]} :  [] }
+          LIMITATIONS.each { |k| @limitations[k] = api_scope[:limitations][k] ? api_scope[:limitations][k].map { |n| n[:name] } : [] }
         end
 
         @exclusions = {}
         if api_scope[:exclusions]
-          @exclusion_keys.each{|k| @exclusions[k] = api_scope[:exclusions][k] ? api_scope[:exclusions][k].map{|n| n[:name]} :  [] }
+          @exclusion_keys.each { |k| @exclusions[k] = api_scope[:exclusions][k] ? api_scope[:exclusions][k].map { |n| n[:name] } : [] }
         end
 
         @container = nil
-
-      end  # init
+      end # init
 
       ###
       ### Set the scope's inclusions to all targets.
@@ -245,18 +239,17 @@ module JSS
       ###
       def include_all(clear = false)
         @inclusions = {}
-        @inclusion_keys.each{|k| @inclusions[k] = []}
+        @inclusion_keys.each { |k| @inclusions[k] = [] }
         @all_targets = true
         if clear
           @limitations = {}
-          LIMITATIONS.each{|k| @limitations[k] = []}
+          LIMITATIONS.each { |k| @limitations[k] = [] }
 
           @exclusions = {}
-          @exclusion_keys.each{|k| @exclusions[k] = []}
+          @exclusion_keys.each { |k| @exclusions[k] = [] }
         end
         @container.should_update if @container
       end
-
 
       ###
       ### Replace a list of item names for inclusion in this scope.
@@ -275,7 +268,7 @@ module JSS
       ###
       def set_inclusion(key, list)
         raise JSS::InvalidDataError, "Inclusion key must be one of :#{@inclusion_keys.join(', :')}" unless @inclusion_keys.include? key
-        raise JSS::InvalidDataError, "List must be an Array of #{key} names, it may be empty." unless list.kind_of? Array
+        raise JSS::InvalidDataError, "List must be an Array of #{key} names, it may be empty." unless list.is_a? Array
 
         return nil if list.sort == @inclusions[key].sort
 
@@ -283,7 +276,7 @@ module JSS
         if list.empty?
           @inclusion[key] = list
           # if ALL the @inclusion keys are empty, then set all targets to true.
-          @all_targets =  @inclusions.values.reject{|a| a.nil? or a.empty?}.empty?
+          @all_targets =  @inclusions.values.reject { |a| a.nil? || a.empty? }.empty?
           @container.should_update if @container
           return list
         end
@@ -291,14 +284,13 @@ module JSS
         ### check the names
         list.each do |name|
           raise JSS::NoSuchItemError, "No existing #{key} with name '#{name}'" unless check_name key, name
-          raise JSS::AlreadyExistsError, "Can't set #{key} scope to '#{name}' because it's already an explicit exclusion." if @exclusions[key] and @exclusions[key].include? name
+          raise JSS::AlreadyExistsError, "Can't set #{key} scope to '#{name}' because it's already an explicit exclusion." if @exclusions[key] && @exclusions[key].include?(name)
         end # each
 
         @inclusions[key] = list
         @all_targets = false
         @container.should_update if @container
       end # sinclude_in_scope
-
 
       ###
       ### Add a single item for this inclusion in this scope.
@@ -314,16 +306,15 @@ module JSS
       ###
       ### @return [void]
       ###
-      def add_inclusion (key, item)
+      def add_inclusion(key, item)
         raise JSS::InvalidDataError, "Inclusion key must be one of :#{@inclusion_keys.join(', :')}" unless @inclusion_keys.include? key
-        raise JSS::InvalidDataError, "Item must be a #{key} name." unless item.kind_of? String
+        raise JSS::InvalidDataError, "Item must be a #{key} name." unless item.is_a? String
 
-        return nil if @inclusions[key] and @inclusions[key].include? item
+        return nil if @inclusions[key] && @inclusions[key].include?(item)
 
         ### check the name
         raise JSS::NoSuchItemError, "No existing #{key} with name '#{item}'" unless check_name key, item
-        raise JSS::AlreadyExistsError, "Can't set #{key} scope to '#{item}' because it's already an explicit exclusion." if @exclusions[key] and @exclusions[key].include? item
-
+        raise JSS::AlreadyExistsError, "Can't set #{key} scope to '#{item}' because it's already an explicit exclusion." if @exclusions[key] && @exclusions[key].include?(item)
 
         @inclusions[key] << item
         @all_targets = false
@@ -342,20 +333,19 @@ module JSS
       ###
       ### @return [void]
       ###
-      def remove_inclusion (key, item)
+      def remove_inclusion(key, item)
         raise JSS::InvalidDataError, "Inclusion key must be one of :#{@inclusion_keys.join(', :')}" unless @inclusion_keys.include? key
-        raise JSS::InvalidDataError, "Item must be a #{key} name." unless item.kind_of? String
+        raise JSS::InvalidDataError, "Item must be a #{key} name." unless item.is_a? String
 
-        return nil unless @inclusions[key] and @inclusions[key].include? item
+        return nil unless @inclusions[key] && @inclusions[key].include?(item)
 
         @inclusions[key] -= [item]
 
         # if ALL the @inclusion keys are empty, then set all targets to true.
-        @all_targets =  @inclusions.values.reject{|a| a.nil? or a.empty?}.empty?
+        @all_targets = @inclusions.values.reject { |a| a.nil? || a.empty? }.empty?
 
         @container.should_update if @container
       end
-
 
       ###
       ### Replace a limitation list for this scope.
@@ -374,9 +364,9 @@ module JSS
       ###
       ### @todo  handle ldap user group lookups
       ###
-      def set_limitation (key, list)
+      def set_limitation(key, list)
         raise JSS::InvalidDataError, "Limitation key must be one of :#{LIMITATIONS.join(', :')}" unless LIMITATIONS.include? key
-        raise JSS::InvalidDataError, "List must be an Array of #{key} names, it may be empty." unless list.kind_of? Array
+        raise JSS::InvalidDataError, "List must be an Array of #{key} names, it may be empty." unless list.is_a? Array
         return nil if list.sort == @limitations[key].sort
 
         if list.empty?
@@ -387,14 +377,13 @@ module JSS
 
         ### check the names
         list.each do |name|
-          raise JSS::NoSuchItemError, "No existing #{key} with name '#{name}'" unless check_name key,  name
-          raise JSS::AlreadyExistsError, "Can't set #{key} limitation for '#{name}' because it's already an explicit exclusion." if @exclusions[key] and @exclusions[key].include? name
+          raise JSS::NoSuchItemError, "No existing #{key} with name '#{name}'" unless check_name key, name
+          raise JSS::AlreadyExistsError, "Can't set #{key} limitation for '#{name}' because it's already an explicit exclusion." if @exclusions[key] && @exclusions[key].include?(name)
         end # each
 
         @limitations[key] = list
         @container.should_update if @container
       end # limit scope
-
 
       ###
       ### Add a single item for limiting this scope.
@@ -412,16 +401,15 @@ module JSS
       ###
       ### @todo  handle ldap user/group lookups
       ###
-      def add_limitation (key, item)
+      def add_limitation(key, item)
         raise JSS::InvalidDataError, "Limitation key must be one of :#{LIMITATIONS.join(', :')}" unless LIMITATIONS.include? key
-        raise JSS::InvalidDataError, "Item must be a #{key} name." unless item.kind_of? String
+        raise JSS::InvalidDataError, "Item must be a #{key} name." unless item.is_a? String
 
-        return nil if @limitations[key] and @limitations[key].include? item
+        return nil if @limitations[key] && @limitations[key].include?(item)
 
         ### check the name
         raise JSS::NoSuchItemError, "No existing #{key} with name '#{item}'" unless check_name key, item
-        raise JSS::AlreadyExistsError, "Can't set #{key} limitation for '#{name}' because it's already an explicit exclusion." if @exclusions[key] and @exclusions[key].include? item
-
+        raise JSS::AlreadyExistsError, "Can't set #{key} limitation for '#{name}' because it's already an explicit exclusion." if @exclusions[key] && @exclusions[key].include?(item)
 
         @limitations[key] << item
         @container.should_update if @container
@@ -441,19 +429,16 @@ module JSS
       ###
       ### @todo  handle ldap user/group lookups
       ###
-      def remove_limitation( key, item)
+      def remove_limitation(key, item)
         raise JSS::InvalidDataError, "Limitation key must be one of :#{LIMITATIONS.join(', :')}" unless LIMITATIONS.include? key
-        raise JSS::InvalidDataError, "Item must be a #{key} name." unless item.kind_of? String
+        raise JSS::InvalidDataError, "Item must be a #{key} name." unless item.is_a? String
 
-        return nil unless @limitations[key] and @limitations[key].include? item
+        return nil unless @limitations[key] && @limitations[key].include?(item)
 
         @limitations[key] -= [item]
         @container.should_update if @container
-      end
+      end ###
 
-
-
-      ###
       ### Replace an exclusion list for this scope
       ###
       ### The list must be an Array of names of items of the Class being excluded from the scope
@@ -468,9 +453,9 @@ module JSS
       ###
       ### @return [void]
       ###
-      def set_exclusion (key, list)
+      def set_exclusion(key, list)
         raise JSS::InvalidDataError, "Exclusion key must be one of :#{@exclusion_keys.join(', :')}" unless @exclusion_keys.include? key
-        raise JSS::InvalidDataError, "List must be an Array of #{key} names, it may be empty." unless list.kind_of? Array
+        raise JSS::InvalidDataError, "List must be an Array of #{key} names, it may be empty." unless list.is_a? Array
         return nil if list.sort == @exclusions[key].sort
 
         if list.empty?
@@ -483,12 +468,11 @@ module JSS
         list.each do |name|
           raise JSS::NoSuchItemError, "No existing #{key} with name '#{name}'" unless check_name key, name
           case key
-            when *@inclusion_keys
-              raise JSS::AlreadyExistsError, "Can't exclude #{key} '#{name}' because it's already explicitly included." if  @inclusions[key] and @inclusions[key].include? name
-            when *LIMITATIONS
-              raise JSS::AlreadyExistsError, "Can't exclude #{key} '#{name}' because it's already an explicit limitation." if @limitations[key] and @limitations[key].include? name
+          when *@inclusion_keys
+            raise JSS::AlreadyExistsError, "Can't exclude #{key} '#{name}' because it's already explicitly included." if @inclusions[key] && @inclusions[key].include?(name)
+          when *LIMITATIONS
+            raise JSS::AlreadyExistsError, "Can't exclude #{key} '#{name}' because it's already an explicit limitation." if @limitations[key] && @limitations[key].include?(name)
           end
-
         end # each
 
         @exclusions[key] = list
@@ -509,16 +493,16 @@ module JSS
       ###
       ### @return [void]
       ###
-      def add_exclusion (key, item)
+      def add_exclusion(key, item)
         raise JSS::InvalidDataError, "Exclusion key must be one of :#{@exclusion_keys.join(', :')}" unless @exclusion_keys.include? key
-        raise JSS::InvalidDataError, "Item must be a #{key} name." unless item.kind_of? String
+        raise JSS::InvalidDataError, "Item must be a #{key} name." unless item.is_a? String
 
-        return nil if @exclusions[key] and @exclusions[key].include? item
+        return nil if @exclusions[key] && @exclusions[key].include?(item)
 
         ### check the name
         raise JSS::NoSuchItemError, "No existing #{key} with name '#{item}'" unless check_name key, item
-        raise JSS::AlreadyExistsError, "Can't exclude #{key} scope to '#{item}' because it's already explicitly included." if @inclusions[key] and @inclusions[key].include? item
-        raise JSS::AlreadyExistsError, "Can't exclude #{key} '#{item}' because it's already an explicit limitation." if @limitations[key] and @limitations[key].include? item
+        raise JSS::AlreadyExistsError, "Can't exclude #{key} scope to '#{item}' because it's already explicitly included." if @inclusions[key] && @inclusions[key].include?(item)
+        raise JSS::AlreadyExistsError, "Can't exclude #{key} '#{item}' because it's already an explicit limitation." if @limitations[key] && @limitations[key].include?(item)
 
         @exclusions[key] << item
         @container.should_update if @container
@@ -536,11 +520,11 @@ module JSS
       ###
       ### @return [void]
       ###
-      def remove_exclusion (key, item)
+      def remove_exclusion(key, item)
         raise JSS::InvalidDataError, "Exclusion key must be one of :#{@exclusion_keys.join(', :')}" unless @exclusion_keys.include? key
-        raise JSS::InvalidDataError, "Item must be a #{key} name." unless item.kind_of? String
+        raise JSS::InvalidDataError, "Item must be a #{key} name." unless item.is_a? String
 
-        return nil unless @exclusions[key] and @exclusions[key].include? item
+        return nil unless @exclusions[key] && @exclusions[key].include?(item)
 
         @exclusions[key] -= [item]
         @container.should_update if @container
@@ -554,33 +538,31 @@ module JSS
       ### @return [REXML::Element]
       ###
       def scope_xml
-        scope = REXML::Element.new "scope"
+        scope = REXML::Element.new 'scope'
         scope.add_element(@all_key.to_s).text = @all_targets
 
-        @inclusions.each do |klass,list|
-          list_as_hash = list.map{|i| {:name => i} }
-          scope << SCOPING_CLASSES[klass].xml_list( list_as_hash, :name)
+        @inclusions.each do |klass, list|
+          list_as_hash = list.map { |i| { name: i } }
+          scope << SCOPING_CLASSES[klass].xml_list(list_as_hash, :name)
         end
 
         limitations = scope.add_element('limitations')
-        @limitations.each do |klass,list|
-          list_as_hash = list.map{|i| {:name => i} }
-          limitations << SCOPING_CLASSES[klass].xml_list( list_as_hash, :name)
+        @limitations.each do |klass, list|
+          list_as_hash = list.map { |i| { name: i } }
+          limitations << SCOPING_CLASSES[klass].xml_list(list_as_hash, :name)
         end
 
         exclusions = scope.add_element('exclusions')
-        @exclusions.each do |klass,list|
-          list_as_hash = list.map{|i| {:name => i} }
-          exclusions << SCOPING_CLASSES[klass].xml_list( list_as_hash, :name)
+        @exclusions.each do |klass, list|
+          list_as_hash = list.map { |i| { name: i } }
+          exclusions << SCOPING_CLASSES[klass].xml_list(list_as_hash, :name)
         end
-        return scope
-      end #scope_xml
-
+        scope
+      end # scope_xml
 
       ### Aliases
 
       alias all_targets? all_targets
-
 
       #####################################
       ### Private Instance Methods
@@ -594,7 +576,6 @@ module JSS
       ### @return [Boolean] does the name exist for the key in JSS or LDAP?
       ###
       def check_name(key, name)
-
         found_in_jss = SCOPING_CLASSES[key].all_names(api: container.api).include?(name)
 
         return true if found_in_jss
@@ -611,11 +592,11 @@ module JSS
           return true
         end # begin
 
-        return false
+        false
       end
 
-
-
     end # class Scope
-  end #module Scopable
+
+  end # module Scopable
+
 end # module
