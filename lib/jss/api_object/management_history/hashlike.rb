d9b7beb3af71509d7a9d652429346cdef11953c9
Chris Lasell <chrisl@pixar.com>
2018-02-19 11:54:37 -0800

    docs and rubocop appeasement

--- lib/jss/api_object/management_history/hashlike.rb
+++ lib/jss/api_object/management_history/hashlike.rb
@@ -6,15 +6,15 @@ module JSS
 
     # This is mixed in to the history event classes to
     # provide hash-like access to their attributes, so that
-    #    `some_event[:date_time]`
+    #    some_event[:date_time]
     # works the same as
-    #     `some_event.date_time`
+    #     some_event.date_time
     # just as with OpenStruct objects
     #
     module HashLike
 
       def [](attr)
-        self.send attr.to_sym
+        send attr.to_sym
       end
 
     end # HashLike
