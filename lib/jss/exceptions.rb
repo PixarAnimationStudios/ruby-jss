722347698405f4963233bbebdd5e8285fc29c7ec
Chris Lasell <chrisl@pixar.com>
2018-06-06 16:49:26 -0700

    cleanup and added some new HTTP-based exceptions

--- lib/jss/exceptions.rb
+++ lib/jss/exceptions.rb
@@ -26,79 +26,82 @@
 ###
 module JSS
 
-  #####################################
   ### Exceptions
   #####################################
 
-  ###
   ### MissingDataError - raise this error when we
   ### are missing args, or other simliar stuff.
   ###
   class MissingDataError < RuntimeError; end
 
-  ###
   ### InvalidDataError - raise this error when
   ### a data item isn't what we expected.
   ###
   class InvalidDataError < RuntimeError; end
 
-  ###
   ### InvalidConnectionError - raise this error when we
   ### don't have a usable connection to a network service, or
   ### don't have proper authentication/authorization.
   ###
   class InvalidConnectionError < RuntimeError; end
 
-  ###
   ### NoSuchItemError - raise this error when
   ### a desired item doesn't exist.
   ###
   class NoSuchItemError < RuntimeError; end
 
-  ###
   ### AlreadyExistsError - raise this error when
   ### trying to create something that already exists.
   ###
   class AlreadyExistsError < RuntimeError; end
 
-  ###
   ### FileServiceError - raise this error when
   ### there's a problem accessing file service on a
   ### distribution point.
   ###
   class FileServiceError < RuntimeError; end
 
-  ###
   ### UnmanagedError - raise this when we
   ### try to do something managerial to
   ### an unmanaged object
   ###
-  class UnmanagedError <  RuntimeError; end
+  class UnmanagedError < RuntimeError; end
 
-  ###
   ### UnsupportedError - raise this when we
   ### try to do something not yet supported
   ###
-  class UnsupportedError <  RuntimeError; end
+  class UnsupportedError < RuntimeError; end
 
-  ###
   ### TimeoutError - raise this when we
   ### try to do and it times out
   ###
-  class TimeoutError <  RuntimeError; end
+  class TimeoutError < RuntimeError; end
 
-  ###
   ### AuthenticationError - raise this when
   ### a name/pw are wrong
   ###
-  class AuthenticationError <  RuntimeError; end
+  class AuthenticationError < RuntimeError; end
 
-  ###
   ### ConflictError - raise this when
   ### attempts to PUT or PUSH to the API
   ### result in a 409 Conflict http error.
-  ### See JSS::APIConnect.instance.raise_conflict_error
+  ### See {JSS::APIConnection#raise_conflict_error}
   ###
-  class ConflictError <  RuntimeError; end
+  class ConflictError < RuntimeError; end
 
-end  # module JSS
+  ### BadRequestError - raise this when
+  ### attempts to PUT or PUSH or DELETE to the API
+  ### result in a 400 Bad Request http error.
+  ### See {JSS::APIConnection.raise_bad_request_error}
+  ###
+  class BadRequestError < RuntimeError; end
+
+  ### APIRequestError - raise this when
+  ### attempts API actions generate an error not dealt with
+  ### by ConflictError or BadRequestError
+  ### result in a 400 Bad Request http error.
+  ### See {JSS::APIConnection.raise_api_error}
+  ###
+  class APIRequestError < RuntimeError; end
+
+end # module JSS
