4c3b61b27788ccc6cae615358037fc4aa9ab20c7
Chris Lasell <chrisl@pixar.com>
2018-06-06 16:51:58 -0700

    PatchPolicy - getting closer

--- lib/jss/api_object/patch_policy.rb
+++ lib/jss/api_object/patch_policy.rb
@@ -46,6 +46,10 @@ module JSS
 
     SCOPE_TARGET_KEY = :computers
 
+    AUTO_INSTALL_GRACE_PERIOD_MESSAGE = '$APP_NAMES will quit in $DELAY_MINUTES minutes so that $SOFTWARE_TITLE can be updated. Save anything you are working on and quit the app(s).'.freeze
+
+    DFT_ENABLED = false
+
 
     USE_XML_WORKAROUND = {
       patch_policy: {
@@ -194,6 +198,11 @@ module JSS
           deadlines: {
             deadline_enabled: nil,
             deadline_period: 7
+          },
+          grace_period: {
+            grace_period_duration: 15,
+            notification_center_subject: 'Important',
+            message: AUTO_INSTALL_GRACE_PERIOD_MESSAGE
           }
         },
         software_title_configuration_id: 2
@@ -244,48 +253,220 @@ module JSS
     # @return [Integer] If a deadline is enabled, how many days is it?
     attr_reader :deadline_period
 
+    # @return [Integer] How many minutes does the user have to quit the killapps?
+    attr_reader :grace_period_duration
+
+    # @return [String] The Subject of the message displayed asking the user to
+    #    quit the killapps within @grace_period_duration minutes
+    attr_reader :grace_period_notification_center_subject
+
+    # @return [String] The message displayed asking the user to quit the killapps
+    #   within @grace_period_duration minutes
+    attr_reader :grace_period_message
+
     # @return [Integer] the id of the JSS::PatchTitle for this policy
     attr_reader :software_title_configuration_id
     alias software_title_id software_title_configuration_id
     alias patch_title_id software_title_configuration_id
 
+    # When making new Patch Polices :patch_title and :target_version must be
+    # provided as well as :name.
+    #
+    # :patch_title is the name or id of a currently active patch title
+    #
+    # :target_version is the string identfier of an available version of
+    # the title. The target version MUST have a package assigned to it.
+    #
     def initialize(data = {})
       super
-      @enabled = @init_data[:general][:enabled]
-      @target_version = @init_data[:general][:target_version]
-      @release_date = JSS.epoch_to_time @init_data[:general][:release_date]
-      @incremental_update = @init_data[:general][:incremental_update]
-      @reboot = @init_data[:general][:reboot]
-      @minimum_os = @init_data[:general][:minimum_os]
-      @kill_apps = @init_data[:general][:kill_apps]
-      @allow_downgrade = @init_data[:general][:allow_downgrade]
-      @patch_unknown = @init_data[:general][:patch_unknown]
-      @deadline_enabled = @init_data[:user_interaction][:deadlines][:deadline_enabled]
-      @deadline_period = @init_data[:user_interaction][:deadlines][:deadline_period]
+
+      gen = @init_data[:general]
+      gen ||= {}
+
+      if !in_jss
+        raise JSS::MissingDataError, ':patch_title required when creating a patch policy' unless @init_data[:patch_title]
+        title_id = JSS::PatchTitle.valid_id @init_data[:patch_title]
+        raise JSS::NoSuchItemError, "No Patch Title matches '#{@init_data[:patch_title]}'" unless title_id
+        @init_data[:software_title_configuration_id] = title_id
+        validate_target_version @init_data[:target_version]
+        @init_data[:general][:target_version] = @init_data[:target_version]
+      end
+
+
+      @enabled = gen[:enabled]
+      @target_version = gen[:target_version]
+      @allow_downgrade = gen[:allow_downgrade]
+      @patch_unknown = gen[:patch_unknown]
+
+
+      @init_data[:user_interaction] ||= {}
+      deadlines = @init_data[:user_interaction][:deadlines]
+      deadlines ||= {}
+      grace = @init_data[:user_interaction][:grace_period]
+      grace ||= {}
+
+      @deadline_enabled = deadlines[:deadline_enabled]
+      @deadline_period = deadlines[:deadline_period]
+
+      @grace_period_duration = grace[:grace_period_duration]
+      @grace_period_notification_center_subject = grace[:notification_center_subject]
+      @grace_period_message = grace[:message]
+
+      # This is read only - even if you put a change, it's ignored.
+      @software_title_configuration_id = @init_data[:software_title_configuration_id]
+
+      # read-only values, they come from the version.
+      @release_date = JSS.epoch_to_time gen[:release_date]
+      @incremental_update = gen[:incremental_update]
+      @reboot = gen[:reboot]
+      @minimum_os = gen[:minimum_os]
+      @kill_apps = gen[:kill_apps]
 
     end
 
+    # Set a new target version for this policy.
+    # The version must exist in the policy's PatchTitle, and have a package
+    # assigned to it.
+    #
+    # @param new_tgt_vers[String] the new version for this Patch Policy.
+    #
+    # @return [void]
+    #
+    def target_version=(new_tgt_vers)
+      return if new_tgt_vers == target_version
+      validate_target_version new_tgt_vers
+      @target_version = new_tgt_vers
+      @need_to_update = true
+      @refetch_for_new_version = true
+    end
+
+    # The JSS::PatchTitle to for this PatchPolicy
+    #
+    # @param refresh [Boolean] Should the Title be re-fetched from the API?
+    #
+    # @return [JSS::PatchTitle, nil]
+    #
+    def patch_title(refresh = false)
+      return nil unless JSS::PatchTitle.all_ids.include? software_title_configuration_id
+      @patch_title = nil if refresh
+      @patch_title ||= JSS::PatchTitle.fetch id: software_title_configuration_id
+    end
+
+    # @return [String] the name of the PatchTitle for this patch policy
+    def patch_title_name
+      return @patch_title.name if @patch_title
+      JSS::PatchTitle.map_all_ids_to(:name)[software_title_configuration_id]
+    end
+
+    # Set the downgradability of this policy - i.e. can it run when
+    # the installed version is newer than this one?
+    #
+    # @param new_val [Boolean] Can this policy be used for downgrades?
+    #
+    # @return [void]
+    #
+    def allow_downgrade=(new_val)
+      return if new_val == allow_downgrade
+      raise JSS::InvalidDataError, 'New value must be boolean true or false' unless JSS::TRUE_FALSE.include? new_val
+      @allow_downgrade = new_val
+      @need_to_update = true
+    end
+
+    # Set the abillity of this policy to install when the previosly installed
+    # version cannot be determined?
+    #
+    # @param new_val [Boolean] Can this policy run when we don't know the prev.
+    #  version?
+    #
+    # @return [void]
+    #
+    def patch_unknown=(new_val)
+      return if new_val == patch_unknown
+      raise JSS::InvalidDataError, 'New value must be boolean true or false' unless JSS::TRUE_FALSE.include? new_val
+      @patch_unknown = new_val
+      @need_to_update = true
+    end
+
+    # Set the deadline enforcement of this policy
+    #
+    # @param new_val [Boolean] Does this policy have a deadline for running?
+    #
+    # @return [void]
+    #
+    def deadline_enabled=(new_val)
+      return if new_val == deadline_enabled
+      raise JSS::InvalidDataError, 'New value must be boolean true or false' unless JSS::TRUE_FALSE.include? new_val
+      @deadline_enabled = new_val
+      @need_to_update = true
+    end
+
+    # Set the deadline for running this patch policy to some number of days
+    # after it becomes available.
+    #
+    # @param days[Integer] how many days before this Patch Policy runs automatically?
+    #
+    # @return [void]
+    #
+    def deadline_period=(days)
+      return if deadline_period == days
+      raise JSS::InvalidDataError, 'New value must be an Integer' unless days.is_a? Integer
+      @deadline_period = days
+      @need_to_update = true
+    end
+
+
+    def create
+      super
+      refetch_version_info
+    end
+
+
+    def update
+      super
+      refetch_version_info if @refetch_for_new_version
+      @refetch_for_new_version = false
+    end
+
+
+
+    # Private Instance Methods
+    #####################################
     private
 
+    def validate_target_version(tgt_vers)
+      raise JSS::InvalidDataError, 'target_version must be a String' unless tgt_vers.is_a? String
+      raise JSS::InvalidDataError, 'target_version must not be empty' if tgt_vers.empty?
+
+      unless title.versions.key? new_tgt_vers
+        raise JSS::UnsupportedError, "Version '#{new_tgt_vers}' does not exist for title: #{patch_title_name}."
+      end
+
+      unless title.versions_with_packages.key? new_tgt_vers
+        raise JSS::UnsupportedError, "Version '#{new_tgt_vers}' cannot be used in Patch Policies until a package is assigned to it."
+      end
+    end
+
+    def refetch_version_info
+      tmp = self.class.fetch id: id
+      @release_date = tmp.release_date
+      @incremental_update = tmp.incremental_update
+      @reboot = tmp.reboot
+      @minimum_os = tmp.minimum_os
+      @kill_apps = tmp.kill_apps
+    end
+
     def rest_xml
-      doc = REXML::Document.new APIConnection::XML_HEADER
+      doc = REXML::Document.new JSS::APIConnection::XML_HEADER
       obj = doc.add_element RSRC_OBJECT_KEY.to_s
 
       general = obj.add_element 'general'
       general.add_element('name').text = @name
       general.add_element('enabled').text = @enabled
       general.add_element('target_version').text = @target_version
-      general.add_element('release_date').text = @release_date.to_jss_epoch
-      general.add_element('incremental_update').text = @incremental_update
-      general.add_element('reboot').text = @reboot
-      general.add_element('minimum_os').text = @minimum_os
       general.add_element('allow_downgrade').text = @allow_downgrade
       general.add_element('patch_unknown').text = @patch_unknown
-      killapps_elem = general.add_element('kill_apps')
-      @kill_apps.each do |ka|
-        killapp_elem = killapps_elem.add_element('kill_app')
-        JSS.hash_to_rexml_array(ka).each { |data| killapp_elem << data }
-      end
+
+      obj << scope.scope_xml
 
       add_self_service_xml doc
 
