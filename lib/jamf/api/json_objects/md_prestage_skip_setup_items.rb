eb4bfd73a88b1fcfae79c3b1ebb7b97cc22185d9
Chris Lasell <chrisl@pixar.com>
2020-10-29 17:31:45 -0700

    Update prestage skip setup items

--- lib/jamf/api/json_objects/md_prestage_skip_setup_items.rb
+++ lib/jamf/api/json_objects/md_prestage_skip_setup_items.rb
@@ -26,7 +26,34 @@
 # The module
 module Jamf
 
-  # A 'location' for a computer prestage in Jamf Pro
+  # The 'skip Setup Items' for a mobile device prestage in Jamf Pro
+  # The ones in common with computers are in the superclass
+  # To see the ones that should be here, remove anything that's in
+  # computers's list from the device ones, thus:
+  #
+  # (Jamf::MobileDevicePrestage.all.sample[:skipSetupItems].keys - Jamf::ComputerPrestage.all.sample[:skipSetupItems].keys).sort
+  #  => [:Android,
+  # :CloudStorage,
+  # :ExpressLanguage,
+  # :HomeButtonSensitivity,
+  # :OnBoarding,
+  # :Passcode,
+  # :PreferredLanguage,
+  # :RestoreCompleted,
+  # :SIMSetup,
+  # :ScreenSaver,
+  # :SoftwareUpdate,
+  # :TVHomeScreenSync,
+  # :TVProviderSignIn,
+  # :TVRoom,
+  # :TapToSetup,
+  # :TransferData,
+  # :UpdateCompleted,
+  # :WatchMigration,
+  # :Welcome,
+  # :Zoom,
+  # :iMessageAndFaceTime]
+  #
   class MobileDevicePrestageSkipSetupItems < Jamf::PrestageSkipSetupItems
 
     OBJECT_MODEL = superclass::OBJECT_MODEL.merge(
@@ -87,6 +114,14 @@ module Jamf
         aliases: %i[preferredLanguage preferredlanguage preferred_language]
       },
 
+      # @!attribute RestoreCompleted
+      #   @return [Boolean]
+      RestoreCompleted: {
+        class: :boolean,
+        aliases: %i[restorecompleted restoreCompleted restore_completed]
+      },
+
+
       # @!attribute SIMSetup
       #   @return [Boolean]
       SIMSetup: {
@@ -134,6 +169,20 @@ module Jamf
       TapToSetup: {
         class: :boolean,
         aliases: %i[tapToSetup taptosetup tap_to_setup]
+      },
+
+      # @!attribute TransferData
+      #   @return [Boolean]
+      TransferData: {
+        class: :boolean,
+        aliases: %i[transferData transferdata transfer_data]
+      },
+
+      # @!attribute UpdateCompleted
+      #   @return [Boolean]
+      UpdateCompleted: {
+        class: :boolean,
+        aliases: %i[updateCompleted updatecompleted update_completed]
       },
 
       # @!attribute WatchMigration
