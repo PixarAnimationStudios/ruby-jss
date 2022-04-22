bfa11bd63f77eea6c6ab4a9fa791109ee18f21a9
Chris Lasell <chrisl@pixar.com>
2022-04-22 14:48:49 -0700

    initial tests for AdvancedSearches

--- test/tests/advanced_user_search.rb
+++ test/tests/advanced_user_search.rb
@@ -23,50 +23,14 @@
 ###
 ###
 
-# Methods and constants for the tests
 module JamfTest
 
-  TOP_PREFIX = '>>>>>'.freeze
-  TEST_PREFIX = '-->'.freeze
+  class AdvancedUserSearch < JamfTest::APITest
 
-  ERROR_PREFIX1 = '****-> ERROR in :'.freeze
-  ERROR_PREFIX2 = '****->   '.freeze
+    include JamfTest::AdvancedSearchTests
 
-  def self.say(msg, from: :top)
-    msgs = msg.is_a?(Array) ? msg : [msg]
-    prefix =
-      if from == :top
-        TOP_PREFIX
-      else
-        "- #{from} #{TEST_PREFIX}"
-      end
-    msgs.each { |m| puts "#{prefix} #{m}" }
-  end
+    COLLECTION_CLASS = Jamf::AdvancedUserSearch
 
-  def self.report(a_problem)
-    if a_problem.is_a? Exception
-      puts caller_locations
-      src = caller_locations(5..5).first
-      msg = a_problem.to_s
-    else
-      src = caller_locations(3..3).first
-      msg = a_problem
-    end
-    location = "method '#{src.label}' at line #{src.lineno} of #{File.basename src.path}"
-    @errors << "#{location}: #{msg}"
-    puts " #{ERROR_PREFIX1} #{location}"
-    puts " #{ERROR_PREFIX2} #{msg}"
-  end
+  end # class
 
 end # module JamfTest
-
-# load in the rest of the module
-libdir = Pathname.new File.dirname(__FILE__)
-moduledir = libdir + 'jamf_test'
-
-# order matters, of course
-require "#{moduledir}/auth"
-require "#{moduledir}/APITest"
-require "#{moduledir}/collection_tests"
-require "#{moduledir}/prestage_tests"
-require "#{moduledir}/advanced_search"
