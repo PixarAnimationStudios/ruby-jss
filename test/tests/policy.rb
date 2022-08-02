ba2957941a39e5cf729108e55ae3ba6bbbac356a
Chris Lasell <chrisl@pixar.com>
2022-08-02 12:08:44 -0700

    policy tests, still need work

--- test/tests/policy.rb
+++ test/tests/policy.rb
@@ -35,22 +35,26 @@ module JamfTest
 
     # The main test method for this class
     def run_class_tests
-      # we will override the object tests
+      # policies are special so we define all the object tests here 
       run_collection_tests do_object_tests: false
 
-      # create_new_pol
-      # add_data_to_new_pol
+      create_new
+
+      add_data_to_new
+
+      # save_new
+      # fetch_new
+      # validate_fetched
+      # modify_fetched
+      # re_save_fetched
+      # re_fetch
+      # validate_changes
+      # delete
+      # confirm_deleted
     end
 
     #################
-    def create_new_pol
-      @test_pol_name = "#{TEST_OBJECT_BASE_NAME}-#{Jamf.cnx.user}-#{Time.now.to_i}"
-      @unsaved_new_pol = Jamf::Policy.create name: @test_pol_name
-      say "Created new policy instance with name #{@test_pol_name} and no other attribs"
-    end
-
-    #################
-    def add_data_to_new_pol
+    def add_data_to_new
       set_new_pol_category
       set_new_pol_pkg
       set_new_pol_trigger
@@ -59,92 +63,77 @@ module JamfTest
 
     #################
     def set_new_pol_category
-      # DEPRECATED - should raise an error, but eventually will not
+      # DEPRECATED - should raise an error, but eventually will not until saved.
       begin
-        @unsaved_new_pol.category = 'foo-this-is-not-a-real-category'
+        @unsaved_new_object.category = 'foo-this-is-not-a-real-category'
       rescue Jamf::NoSuchItemError
         nil
       end
-      @unsaved_new_pol.category = pol_category
+      @unsaved_new_object.category = pol_category
       say "Set policy category to: #{pol_category}"
     end
 
     #################
     def set_new_pol_pkg
-      # DEPRECATED - should raise an error, but eventually will not
+      # DEPRECATED - should raise an error, but eventually will not until saved
       begin
-        @unsaved_new_pol.add_package 'foo'
+        @unsaved_new_object.add_package 'foo'
       rescue Jamf::NoSuchItemError
         nil
       end
-      @pkg_name = Jamf::Package.all_names.sample
-      @unsaved_new_pol.add_package @pkg_name
-      say "Added policy pkg: #{@pkg_name}"
+      @unsaved_new_object.add_package pol_pkg
+      say "Added policy pkg: #{pol_pkg}"
     end
 
     #################
     def set_new_pol_trigger
-      @unsaved_new_pol.set_trigger_event :custom, CUSTOM_TRIGGER
+      @unsaved_new_object.set_trigger_event :custom, CUSTOM_TRIGGER
       say "Set policy trigger to: #{CUSTOM_TRIGGER}"
     end
 
     #################
     def set_new_pol_scope
-      @unsaved_new_pol.scope.add_target :computer, target_computer_name
+      @unsaved_new_object.scope.add_target :computer, target_computer_name
       say "Set policy scope targets to: #{@unsaved_new_pol.scope.targets}"
-      @unsaved_new_pol.scope.add_exclusion :computer_group, excluded_group_name
+      @unsaved_new_object.scope.add_exclusion :computer_group, excluded_group_name
       say "Set policy scope targets to: #{@unsaved_new_pol.scope.exclusions}"
     end
 
+    #################
+    def save_new_pol; end
 
     #################
-    def save_new_pol
-
-    end
+    def fetch_new_pol; end
 
     #################
-    def fetch_new_pol
-
-    end
+    def validate_new_pol; end
 
     #################
-    def validate_new_pol
-
-    end
+    def modify_pol; end
 
     #################
-    def modify_pol
-
-    end
+    def save_pol; end
 
     #################
-    def save_pol
-
-    end
+    def re_fetch_pol; end
 
     #################
-    def re_fetch_pol
-
-    end
+    def validate_changes; end
 
     #################
-    def validate_changes
-
-    end
+    def delete_pol; end
 
     #################
-    def delete_pol
-
-    end
-
-    #################
-    def confirm_deleted
-
-    end
+    def confirm_deleted; end
 
     #######
     def pol_category
       @pol_category ||= Jamf::Category.all_names.sample
+    end
+
+    #######
+    def pol_pkg
+      @pol_pkg ||= Jamf::Package.all_names.sample
     end
 
     ########
@@ -156,7 +145,6 @@ module JamfTest
     def excluded_group_name
       @excluded_group_name ||= Jamf::ComputerGroup.all_names.sample
     end
-
 
   end # class
 
