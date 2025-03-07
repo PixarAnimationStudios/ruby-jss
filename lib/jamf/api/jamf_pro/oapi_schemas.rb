6ea7c9d6e9929c351e3becc2d7e73a77db5d250d
Chris Lasell <chrisl@pixar.com>
2025-03-07 13:54:02 -0800

    Add docs about the OAPISchemas module

--- lib/jamf/api/jamf_pro/oapi_schemas.rb
+++ lib/jamf/api/jamf_pro/oapi_schemas.rb
@@ -25,8 +25,31 @@
 # The module
 module Jamf
 
-  # This module is the namespace for all objects defined
+  # This module is the namespace for all JP API objects defined
   # in the OAPI JSON schema under the components => schemas key
+  #
+  # The classes in this module are the Ruby representations of the
+  # JSON schema objects (Hashes) in the Jamf Pro OAPI schema
+  #
+  # Each class is created from the JSON Object definition and other
+  # schema data, such as required attributes, the class or type of each
+  # attribute (which could be another OAPI object), and any enumurated
+  # values for attributes.
+  # The classes are all subclasses of Jamf::OAPIObject, which defines
+  # common methods for all OAPI objects, such as converting to and from
+  # JSON, and validating attribute values.
+  #
+  # These clases are used in various ways in ruby-jss:
+  # - As the value of an attribute in another OAPI object
+  # - As the parent class for a Jamf Pro API Resource class
+  # - As a representation of a JSON objects sent to or received from the API
+  #   (e.g. in a POST or PUT request)
+  #
+  # They are mostly generated automatically using the 'generate_object_models'
+  # executable in the 'bin' directory of ruby-jss. After generation the files needed
+  # are manually edited to add any needed functionality or clarifications of names
+  # etc, and then they are moved into the 'jamf/api/jamf_pro/oapi_schemas' directory.
+  #
   #####################################
   module OAPISchemas
 
