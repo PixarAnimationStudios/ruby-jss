9b46c2d703a6ff2fdea87f24fef8b61ee6ed2078
Chris Lasell <chrisl@pixar.com>
2025-10-19 13:56:18 -0700

    update comment

--- lib/jamf/api/connection/constants.rb
+++ lib/jamf/api/connection/constants.rb
@@ -55,7 +55,8 @@ module Jamf
       # JamfCloud connections default to 443, not 8443
       JAMFCLOUD_PORT = HTTPS_SSL_PORT
 
-      # The top line of an XML doc for submitting data via Classic API
+      # The top line of an XML doc for submitting data via Classic API,
+      # with an empty root element, for newer versions of REXML.
       XML_HEADER = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><root/>'.freeze
 
       DFT_OPEN_TIMEOUT = 60
